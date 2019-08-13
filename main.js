4991a12ad55972ffa1d424bb27a247cff5a0537f
satoran <53833440+satoran@users.noreply.github.com>
2019-08-13 16:59:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-$(function() {
-  $(function() {
-    $('#menu').scrollspy({ offset: $(window).height() / 2 });
+$(document).ready(function() {
+  var headerHeight = 0;
+  var width = $(window).width();
+  var widthBorder = 500;
+
+  $('a[href^="#"]').click(function() {
+    var speed = 1000;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+
+    if (width <= widthBorder) {
+      headerHeight = 50;
+    }
+    else {
+      headerHeight = 70;
+    }
+
+    var position = target.offset().top - headerHeight;
+
+
+
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
   });
 });
